640024d3da80478631b0f385a0c2a37d1edbd75b
Luke <luke.k.rowell@gmail.com>
2020-02-07 21:02:13 -0500

    Added pg

--- index.js
+++ index.js
@@ -3,6 +3,28 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+async function queryDB(dbConnectionString, queryText, queryValues) {
+    const pool = new Pool({
+        connectionString: dbConnectionString,
+        ssl: true,
+    });
+
+    const client = await pool.connect()
+    const result = await client.query({
+      rowMode: 'array',
+      text: queryText,
+      values: queryValues
+    });
+
+    client.release();
+
+    pool.end(() => {
+        //console.log(`Pool ended for: ${dbConnectionString}`);
+    });
+
+    return result;
+}
+
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 })
